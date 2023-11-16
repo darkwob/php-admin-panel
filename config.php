27bc69d20e9243745cabe1d416eabbbc3e0651ce
DarkWob <71833890+darkwob@users.noreply.github.com>
2023-11-16 10:45:11 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-    $db = new PDO("mysql:host=localhost;dbname=olindeon1_mydatebase;charset=utf8","olindeon1_sado","Senyor2013");
+    $db = new PDO("mysql:host=localhost;dbname=olindeon1_mydatebase;charset=utf8","olindeon1_sado","");
     $db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOExpection $hata) {
     echo $hata -> getMessage();
