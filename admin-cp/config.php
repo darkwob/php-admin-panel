be7a3e85d9faba46130e76b5b762c4947cc9cece
DarkWob <71833890+darkwob@users.noreply.github.com>
2023-11-16 10:46:30 +0300

    Update config.php

--- admin-cp/config.php
+++ admin-cp/config.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 try {
-    $db = new PDO("mysql:host=localhost;dbname=olindeon1_mydatebase;charset=utf8","olindeon1_sado","Senyor2013");
+    $db = new PDO("mysql:host=localhost;dbname=dbname;charset=utf8","username","pass");
     $db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOExpection $hata) {
     echo $hata -> getMessage();
